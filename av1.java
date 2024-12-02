50fb4a7b58e74c7401409c9b5a9289b280092689
gustavin7544 <135707284+gustavin7544@users.noreply.github.com>
2024-12-02 09:03:43 -0300

    Add files via upload

---
+++ av1.java
@@ -0,0 +1,13 @@
+ public class Exer01 {
+  public static void main(string{ args)}
+  Scanner.out.plintln (informe um numero"
+  int num = imput.nexttlin ()
+  if (num=imput
+  Stistem.out.println(String.format ('0 numero %d é negativo, num))
+  } else if (num  0) {
+  Stystem.out.println(String.format("0 numeor%d é positivo,num))
+  {
+else {
+    Stytem.out.printly.format('0 numero informado %d é igualn a 0 ", num))
+
+  
